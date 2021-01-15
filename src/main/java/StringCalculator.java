587f87eedf5699ae3285e0015f830a18f80b281f
adkeaesph <adkeaesph720@gmail.com>
2021-01-15 23:54:59 +0530

    coding for the case of two addends and adding the test cases of multiple addends

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,6 +4,14 @@ public class StringCalculator {
 	public static int add(String numbers) {
 		if(numbers.trim().isEmpty())
 			return 0;
-		return Integer.parseInt(numbers.trim());
+		
+		String[] arrayOfNumbers = numbers.trim().split(",");
+		int firstNumber = Integer.parseInt(arrayOfNumbers[0]);
+		
+		if(arrayOfNumbers.length == 1)
+			return firstNumber;
+		
+		int secondNumber = Integer.parseInt(arrayOfNumbers[1]);
+		return firstNumber + secondNumber;
 	}
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -23,4 +23,16 @@ class StringCalculatorTest {
 		numbers = "1,2";
 		assertEquals(3, StringCalculator.add(numbers));
 	}
+	
+	@Test
+	public void testAdd4() {
+		numbers = "1,2,3";
+		assertEquals(6, StringCalculator.add(numbers));
+	}
+	
+	@Test
+	public void testAdd5() {
+		numbers = "1,2,3,23,10";
+		assertEquals(39, StringCalculator.add(numbers));
+	}
 }
