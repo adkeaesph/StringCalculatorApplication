6b681e73c3dad99b0eed10dfb664613b5271ed9f
adkeaesph <adkeaesph720@gmail.com>
2021-01-15 23:34:05 +0530

    Revert "Coding for the case of one addend and adding the test case of two addends"
    This reverts commit f13238d56c45147b2fa082325e1759b751ec0cf5.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,8 +2,6 @@
 public class StringCalculator {
 	
 	public static int add(String numbers) {
-		if(numbers.trim().isEmpty())
-			return 0;
-		return Integer.parseInt(numbers.trim());
+		return 0;
 	}
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -17,10 +17,4 @@ class StringCalculatorTest {
 		numbers = "1";
 		assertEquals(1, StringCalculator.add(numbers));
 	}
-	
-	@Test
-	public void testAdd3() {
-		numbers = "1,2";
-		assertEquals(3, StringCalculator.add(numbers));
-	}
 }
