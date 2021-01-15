1bb4efbe920a94733df5b0928865687dfedf2af4
adkeaesph <adkeaesph720@gmail.com>
2021-01-15 23:00:38 +0530

    Solving the trivial case of 0 numbers and adding the test case of one addend.

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 class StringCalculatorTest {
 
 	private String numbers;
+	
 	@Test
 	public void testAdd1() {
+		numbers = "";
+		assertEquals(0, StringCalculator.add(numbers));
+	}
+
+	@Test
+	public void testAdd2() {
 		numbers = "1";
 		assertEquals(1, StringCalculator.add(numbers));
 	}
-
 }
